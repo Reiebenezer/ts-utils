46e39dfc99f5aa1ca0a1bd1a99d0be1676bc7aea
Reiebenezer <reiebenezer.duhina@wvsu.edu.ph>
2025-11-26 12:21:21 +0800

    fix: added determiner object for color, updated package to 0.1.2-patch-2

--- src/color.ts
+++ src/color.ts
@@ -1,15 +1,15 @@
 import convert, { type Convert } from 'color-convert';
 
 interface RGB {
-	r: number;
-	g: number;
-	b: number;
+  r: number;
+  g: number;
+  b: number;
 }
 
 interface HSL {
-	h: number;
-	s: number;
-	l: number;
+  h: number;
+  s: number;
+  l: number;
 }
 
 /**
@@ -18,14 +18,17 @@ interface HSL {
 type Hex = `#${string}`;
 
 export interface Color {
-	toString(): string;
-	toJSON(): string;
+  toString(): string;
+  toJSON(): {
+    value: string;
+    __colorType__: true;
+  };
 
-	hsl(useDecimals?: boolean): HSL;
-	hex(): Hex;
-	rgb(): RGB;
+  hsl(useDecimals?: boolean): HSL;
+  hex(): Hex;
+  rgb(): RGB;
 
-	readonly alpha: number;
+  readonly alpha: number;
 }
 
 /**
@@ -38,55 +41,55 @@ export function Color(color: RGB, alpha?: number): Color;
 export function Color(color: HSL, alpha?: number): Color;
 export function Color(color: Hex, alpha?: number): Color;
 export function Color(color: RGB | HSL | Hex, alpha = 255): Color {
-	let _color: Uint8Array;
+  let _color: Uint8Array;
 
-	if (typeof color === 'string' && color.startsWith('#')) {
-		_color = new Uint8Array(convert.hex.rgb(color));
-	}
-	
-	else if (Object.keys(color).every((k) => ['r', 'g', 'b'].includes(k)))
-		_color = new Uint8Array([...Object.values(color)]);
-	
-		else {
-		color = color as HSL;
-		_color = new Uint8Array(convert.hsl.rgb([color.h, color.s, color.l]));
-	}
+  if (typeof color === 'string' && color.startsWith('#')) {
+    _color = new Uint8Array(convert.hex.rgb(color));
+  } else if (Object.keys(color).every((k) => ['r', 'g', 'b'].includes(k)))
+    _color = new Uint8Array([...Object.values(color)]);
+  else {
+    color = color as HSL;
+    _color = new Uint8Array(convert.hsl.rgb([color.h, color.s, color.l]));
+  }
 
-	return {
-		/** @returns the hex value of this class. Includes the alpha value unless specified otherwise */
-		toString() {
-			return this.hex();
-		},
+  return {
+    /** @returns the hex value of this class. Includes the alpha value unless specified otherwise */
+    toString() {
+      return this.hex();
+    },
 
-		toJSON() {
-			return this.hex();
-		},
+    toJSON() {
+      return {
+        value: this.hex(),
+        __colorType__: true,
+      };
+    },
 
-		/** @returns an object with individual h, s, and l values. Includes alpha */
-		hsl(useDecimals = false): HSL {
-			const { hsl } = convert.rgb;
-			const [h, s, l] = useDecimals
-				? hsl.raw(_color[0], _color[1], _color[2])
-				: hsl(_color[0], _color[1], _color[2]);
+    /** @returns an object with individual h, s, and l values. Includes alpha */
+    hsl(useDecimals = false): HSL {
+      const { hsl } = convert.rgb;
+      const [h, s, l] = useDecimals
+        ? hsl.raw(_color[0], _color[1], _color[2])
+        : hsl(_color[0], _color[1], _color[2]);
 
-			return {
-				h,
-				s,
-				l
-			};
-		},
+      return {
+        h,
+        s,
+        l,
+      };
+    },
 
-		hex() {
-			return `#${convert.rgb.hex(_color[0], _color[1], _color[2])}`;
-		},
+    hex() {
+      return `#${convert.rgb.hex(_color[0], _color[1], _color[2])}`;
+    },
 
-		rgb() {
-			const [r, g, b, a] = _color;
-			return { r, g, b, alpha: a };
-		},
+    rgb() {
+      const [r, g, b, a] = _color;
+      return { r, g, b, alpha: a };
+    },
 
-		get alpha() {
-			return alpha;
-		}
-	};
+    get alpha() {
+      return alpha;
+    },
+  };
 }
